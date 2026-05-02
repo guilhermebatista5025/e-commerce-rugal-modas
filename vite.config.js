53430af040d0eb3c05606cbe04633c68268657bf
Guilherme Batista <gmito5025@gmail.com>
2026-05-02 17:58:42 -0300

    acho que foi

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 ADICIONA ISSO
+  base: "/", // 👈 ADICIONA ISSO
   server: {
     port: 5173,
     open: true
